5007b127ec553ff5fac03cb6ff690993903d3327
nitheeshmk5 <22107041@srcas.ac.in>
2024-05-12 10:31:09 +0530

    add lesson1 exercise

---
+++ lesson01-BasicsExercise/main.js
@@ -0,0 +1,29 @@
+//1a
+alert("Good morning");
+//1b
+alert("Nitheeshmk5");
+//1c
+console.log(10 + 5);
+//1d
+console.log(20 - 5);
+//1e
+console.log(2 + 2 - 5);
+//1f
+document.body.innerHTML = "Good morning";
+//1h
+document.body.innerHTML = "Nitheesh";
+
+//Challenge Excercises
+//1h
+let tShirt = 10;
+let socks = 8;
+let plates = 20;
+
+let total = tShirt + socks + plates;
+console.log(total);
+
+//1i
+console.log(100 - 20 - 50 + 200);
+
+//1j
+document.body.innerHTML = " ";
