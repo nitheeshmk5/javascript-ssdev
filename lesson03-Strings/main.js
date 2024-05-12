159e58ad8273f971628d3731fb20cfa178bb62ff
nitheeshmk5 <22107041@srcas.ac.in>
2024-05-12 19:33:31 +0530

    add lesson 03 Strings

---
+++ lesson03-Strings/main.js
@@ -0,0 +1,25 @@
+//3a
+console.log("My name is");
+//3b
+console.log("Nitheesh");
+//3c
+console.log("My name is : " + "Nitheesh");
+//3d,3e
+console.log(`Total cost : $${5 + 3}`);
+//3f
+alert(`Total cost : $${5 + 3}`);
+//3j
+let text = `
+Total cost : $${5 + 6}
+Thanks you,come again!
+`;
+//Challenge
+//3n
+`
+items(4)\t $${4 + 5}
+shipping & handling\t $9.98
+Total before tax\t $${57.88 + 9.98}
+Estimated tax (10%):\t $${(57.88 + 9.98) * 0.1}
+
+Order total:\t $${57.88 + 9.98 + (57.88 + 9.98) * 0.1}
+`;
