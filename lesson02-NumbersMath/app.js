eee3bee9de28c97a7fb085572312453dfff80351
nitheeshmk5 <22107041@srcas.ac.in>
2024-05-12 11:54:01 +0530

    add lesson 02 Numbers

---
+++ lesson02-NumbersMath/app.js
@@ -0,0 +1,30 @@
+// lesson 2
+
+//2a
+console.log(10 + 3 * 8 + 5);
+
+//2b
+console.log((10 + 3 * 8 + 5) / 3);
+
+//2c
+console.log(18.5 + 2 * 7.5);
+
+//2d
+console.log((18.5 + 2 * 7.5) * (10 / 100));
+
+//2e
+console.log((18.5 + 2 * 7.5) * 0.2);
+
+//2k
+Math.ceil(2.2);
+
+//2j
+Math.floor(2.8);
+
+// Challenge
+// 2i
+console.log((25 * 9) / 5 + 32);
+//2m
+console.log(((86 - 32) * 5) / 9);
+//2n
+console.log((-5 * 9) / 5 + 32);
